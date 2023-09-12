b5e88bb263c0290b84727de9a6ea3b46d740fb71
MDAM zZ <76702014+mdamplus@users.noreply.github.com>
2023-09-12 19:35:45 +0200

    Update vacaciones.php
    txt changes

--- vacaciones.php
+++ vacaciones.php
@@ -9,7 +9,7 @@ Author: Tu Nombre
 // Agregar el código JavaScript al footer del sitio
 function vacaciones_script() {
     if (is_home() || is_front_page() || is_product() || is_page()) {
-        $mensaje = "Estimados clientes,\n\nQueremos informarles que en Destilería Martes Santo estaremos disfrutando de unas merecidas vacaciones desde el sábado 9 de septiembre hasta el lunes 18 de septiembre.\n\nDurante este periodo, todos los pedidos que realicen a través de nuestra página web y por este medio serán procesados en el orden en que se reciban a partir del lunes 18 de septiembre.";
+        $mensaje = "Estimados clientes,\n\nQueremos informarles que en [nombre_empresa] estaremos disfrutando de unas merecidas vacaciones desde el [vac_dia1] hasta el [vac_dia2].\n\nDurante este periodo, todos los pedidos que realicen a través de nuestra página web y por este medio serán procesados en el orden en que se reciban.";
         wp_enqueue_script('vacaciones-script', plugin_dir_url(__FILE__) . 'vacaciones.js', array(), '1.0', true);
         wp_localize_script('vacaciones-script', 'vacacionesData', array('mensaje' => $mensaje));
     }
@@ -19,7 +19,7 @@ add_action('wp_enqueue_scripts', 'vacaciones_script');
 // Agregar el div al header
 function vacaciones_header_div() {
     if (is_home() || is_front_page() || is_product() || is_page()) {
-        echo '<div id="vacaciones-header">En Destilería Martes Santo estaremos disfrutando de unas merecidas vacaciones desde el sábado 9 de septiembre hasta el lunes 18 de septiembre. Su pedido se procesará por orden de entrada a partir del día 18. ¡Gracias!</div>';
+        echo '<div id="vacaciones-header">En [nombre_empresa] estaremos disfrutando de unas merecidas vacaciones desde el [vac_dia1] hasta el [vac_dia2]. Su pedido se procesará por orden de entrada. ¡Gracias!</div>';
     }
 }
 add_action('wp_head', 'vacaciones_header_div');
